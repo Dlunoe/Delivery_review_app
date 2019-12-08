b517fdbdfdf6f28135523fc68cb287235f5e5d8e
Dustin Lunoe <dlunoe@yahoo.com>
2019-12-07 22:54:31 -0700

    added mock business

---
+++ delivery-client/src/Components/Home/List/list.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class List extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div className="searchListContainer">
+                <ul>
+                    <li>
+                        <p>business name</p>
+                        <p>address</p>
+                    </li>
+                    <li>
+                        <p>business name</p>
+                        <p>address</p>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+
+}
