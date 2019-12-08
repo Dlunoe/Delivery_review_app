2f1ffc46926381783754e1bef0085adaaa713d66
Dustin Lunoe <dlunoe@yahoo.com>
2019-12-07 21:31:56 -0700

    added containers

--- delivery-client/src/Components/Home/Show/show.jsx
+++ delivery-client/src/Components/Home/Show/show.jsx
@@ -11,17 +11,21 @@ class Show extends Component{
     render(){
         return(
             <div className="showDiv">
-                <h3>Business Name</h3>
-                <p>Business Address</p>
-                <p>Front/Back loading: N/A</p>
-                <ul className="reviewsList">
-                    <li>
-                        <p>Mock review 1</p>
-                    </li>
-                    <li>
-                        <p>Mock review 2</p>
-                    </li>
-                </ul>
+                <div className="businessContainer">
+                    <h3>Business Name</h3>
+                    <p>Business Address</p>
+                    <p>Front/Back loading: N/A</p>
+                </div>
+                <div className="reviewsContainer">
+                    <ul className="reviewsList">
+                        <li>
+                            <p>Mock review 1</p>
+                        </li>
+                        <li>
+                            <p>Mock review 2</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
