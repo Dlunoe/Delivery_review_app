f4a5fb27944c4ba01d6066b97a853e2dd17018b6
Dustin Lunoe <dlunoe@yahoo.com>
2019-12-23 20:45:52 -0700

    added bodyparser and cors to server

--- delivery-api/server.js
+++ delivery-api/server.js
@@ -3,6 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+require('./db/db');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}))
+
 app.listen(3001, () =>{
     console.log('listening on port 3001');
 } )
